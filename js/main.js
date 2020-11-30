7c7c088842b7e5370fe2d5f8cf45962eceee4561
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-11-30 00:04:54 +0000

    refactoring in js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
-let amount = document.querySelector(".form__input--js-amount");
-let currency = document.querySelector(".form__input--js-currency");
-let countButton = document.querySelector(".form__button");
-let resultElement = document.querySelector(".result__paragraph");
-
-let GBP = 5.0239;
-let EUR = 4.4843;
-let USD = 3.7614;
-let CHF = 4.1477;
-
-let result;
-
-countButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let amountPLN = +amount.value;
-    if (currency.value === "GBP") {
-        result = amountPLN / GBP;
-
-    }
-    else if (currency.value === "EUR") {
-        result = amountPLN / EUR;
-
-    }
-    else if (currency.value === "USD") {
-        result = amountPLN / USD;
-
-    }
-    else {
-        result = amountPLN / CHF;
-
+{
+    const calculateAndShowResults = (amountPLN, result, currency) => {
+        const resultElement = document.querySelector(".result__paragraph");
+        resultElement.innerText = `${amountPLN.toFixed(2)} złotych to w przeliczeniu ${result.toFixed(2)} ${currency.value}`;
     }
 
-    resultElement.innerText = `${amountPLN.toFixed(2)} złotych to w przeliczeniu ${result.toFixed(2)} ${currency.value}`;
-    amount.value = "";
-})
+    const resultCalculator = (currency, amountPLN) => {
+        let result;
+        if (currency.value === "GBP") {
+            const GBP = 5.0239;
+            result = amountPLN / GBP;
+
+        }
+        else if (currency.value === "EUR") {
+            const EUR = 4.4843;
+            result = amountPLN / EUR;
+
+        }
+        else if (currency.value === "USD") {
+            const USD = 3.7614;
+            result = amountPLN / USD;
+
+        }
+        else {
+            result = amountPLN / CHF;
+            const CHF = 4.1477;
+
+        };
+        calculateAndShowResults(amountPLN, result, currency)
+    }
+
+    const dataCollectionFromApp = () => {
+        const countButton = document.querySelector(".form__button");
+        countButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            const amount = document.querySelector(".form__input--js-amount");
+            const currency = document.querySelector(".form__input--js-currency");
+            const amountPLN = +amount.value;
+            resultCalculator(currency, amountPLN);
+        });
+    }
+    const init = () => {
+        dataCollectionFromApp();
+    }
+
+    init();
+}
 
